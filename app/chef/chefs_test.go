6f61ef4d322ac54fe0e897bfa2b68e0e1ce81054
Matheus Lucca do Carmo <mematheuslc@gmail.com>
2020-10-09 17:06:35 -0300

    Add professional chef types

--- app/chef/chefs_test.go
+++ app/chef/chefs_test.go
@@ -1,1 +1,24 @@
 package chef
+
+import (
+	"testing"
+)
+
+func TestNewChef(t *testing.T) {
+	n := name("carmel")
+	r := role("professional")
+
+	c, err := NewChef(r, n, "example@example.org")
+
+	if err != nil {
+		t.Errorf("Cannot create a valid chef with valid fields. Some rule changed? Error: %s", err)
+	}
+
+	if c.Name() != n {
+		t.Errorf("Chef was created with wrong name. Expected: %s. Get: %s", n, c.Name())
+	}
+
+	if c.Role() != r {
+		t.Errorf("Chef was created with wrong role. Expected: %s. Get: %s", r, c.Role())
+	}
+}
