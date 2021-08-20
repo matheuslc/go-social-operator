7c023fa26e3b12e66bc35e3e82a7f98b46b59d02
Matheus Lucca do Carmo <matheus.carmo@olxbr.com>
2021-08-19 23:35:34 -0300

    Creating food

--- internal/food/food.go
+++ internal/food/food.go
@@ -1,5 +1,9 @@
 package food
 
+import "github.com/google/uuid"
+
+type Uuid uuid.UUID
+
 // Name defines the name of a food. Food names can have variations,
 // but they need to have an elected name.
 // Cherry Tomato
@@ -13,6 +17,7 @@ type Genus string
 
 // Food defines the food struct and its properties
 type Food struct {
+	Uuid
 	Name
 	Genus
 	Specie
@@ -21,8 +26,8 @@ type Food struct {
 // NewFood creates a new food struct with requireds params
 func NewFood(n Name, g Genus, sp Specie) Food {
 	return Food{
-		n,
-		g,
-		sp,
+		Name:   n,
+		Genus:  g,
+		Specie: sp,
 	}
 }
--- internal/food/repository.go
+++ internal/food/repository.go
@@ -3,6 +3,7 @@ package food
 import (
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
@@ -19,22 +20,23 @@ type Repository struct {
 	Reader
 }
 
-func (repo Repository) Save(f Food) (string, error) {
+func (repo Repository) Save(f Food) (Food, error) {
 	session, err := repo.Db.NewSession(neo4j.SessionConfig{
 		AccessMode:   neo4j.AccessModeWrite,
 		DatabaseName: "neo4j",
 	})
 	if err != nil {
 		fmt.Printf("Cant start a new Neo4j session. Reason: %s", err)
-		return "", nil
+		return Food{}, nil
 	}
 
 	defer session.Close()
 
 	persistedFood, err := session.WriteTransaction(func(transaction neo4j.Transaction) (interface{}, error) {
 		result, err := transaction.Run(
-			"CREATE (f:Food) SET f.name = $name, f.genus = $genus, f.specie RETURN f",
+			"CREATE (f:Food) SET f.uuid = $uuid, f.name = $name, f.genus = $genus, f.specie RETURN f.uuid, f.name, f.genus, f.specie",
 			map[string]interface{}{
+				"uuid":   uuid.New(),
 				"name":   f.Name,
 				"genus":  f.Genus,
 				"specie": f.Specie,
@@ -46,15 +48,20 @@ func (repo Repository) Save(f Food) (string, error) {
 		}
 
 		if result.Next() {
-			return result.Record().GetByIndex(0), nil
+			return Food{
+				Uuid:   Uuid(uuid.MustParse(result.Record().GetByIndex(0).(string))),
+				Name:   Name(result.Record().GetByIndex(1).(string)),
+				Genus:  Genus(result.Record().GetByIndex(2).(string)),
+				Specie: Specie(result.Record().GetByIndex(3).(string)),
+			}, nil
 		}
 
 		return nil, result.Err()
 	})
 
 	if err != nil {
-		return "", err
+		return Food{}, err
 	}
 
-	return persistedFood.(string), nil
+	return persistedFood.(Food), nil
 }
